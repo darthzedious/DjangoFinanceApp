227e461f2611759c70e95e75b756022a073a5763
darthzedious <angel.bobotsov@gmail.com>
2025-01-13 01:28:50 +0200

    adjustment

--- financeDjango/mixins.py
+++ financeDjango/mixins.py
@@ -1,8 +1,6 @@
 import ast
 import json
 
-from financeDjango.annuity_factor_app.helpers import calculate_future_value_annuity_factor_end_year_payment
-
 
 class OperationNameContextMixin:
     operation_name = ''
